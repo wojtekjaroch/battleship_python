fa92a14b824ae8857c085201cbd420c7ba702c93
Przemyslaw-Swiderski <przemyslaw.rafal.swiderski@gmail.com>
2023-01-06 13:09:01 +0100

    nc

--- battleship.py
+++ battleship.py
@@ -1,5 +1,7 @@
 import os
 
+# test działania gita u wojtka
+
 CORRECT_INPUT_OPTION = ['a1', 'a2', 'a3', 'a4', 'a5', 
                         'b1', 'b2', 'b3', 'b4', 'b5', 
                         'c1', 'c2', 'c3', 'c4', 'c5',
